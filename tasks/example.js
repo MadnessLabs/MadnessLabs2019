6776489886e5c9314fa4ab595d016872817b264a
aaronmhappe <fivesenses1982@gmail.com>
2019-06-14 10:57:19 -0500

    create project visualizer map

---
+++ tasks/example.js
@@ -0,0 +1,20 @@
+console.log('this is being run');
+
+// make a component tree, visualizer
+
+// idea#1 -
+
+// 1 - pass in the root component.
+// 2 - parse the document for components within the router
+// 3 - make an object like so
+// { component : {
+//        name: 'app-root',
+//        props: [],
+//				state: [],
+//				events:[],
+//				listeners: [],
+// } 
+//
+// }
+
+// name the roo
