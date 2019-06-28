fb254aefd3286f097455be320ec25e170a894d86
meaustin15 <m.eaustin2015@gmail.com>
2019-06-28 10:56:16 -0500

    ml-post update

--- src/components/ml-latest-post/ml-latest-post.tsx
+++ src/components/ml-latest-post/ml-latest-post.tsx
@@ -1,20 +1,85 @@
-import { Component, h } from '@stencil/core';
-
+import { Component, h} from '@stencil/core';
 
 @Component({
     tag: 'ml-latest-post',
     styleUrl: 'ml-latest-post.css'
 })
-export class MlLatestPost {
+export class MlLatestPost{
 
+  state ={
+    title: null
+  };
+    
+    componentDidMount(){
+        this.XMLHttpRequest();
+        this.fetchData();
+    }
+    
+    XMLHttpRequest(){
+        function reqListener() {
+            var data = JSON.parse(this.responseText);
+            console.log(data);
+          }
+          
+          function reqError(err) {
+            console.log('Fetch Error :-S', err);
+          }
+          
+          var oReq = new XMLHttpRequest();
+          oReq.onload = reqListener;
+          oReq.onerror = reqError;
+          oReq.open('get', 'https://madnesslabs.net/api/v1/feed', true);
+          oReq.send(); 
+    }
+
+    fetchData(){
+        fetch('https://madnesslabs.net/api/v1/feed').then(function(response) {
+        console.log(response.headers.get('author'));
+    console.log(response.headers.get('title'));
+   this.setState( {title: response.headers.get('title')});
+    console.log(response.headers.get('link'));
+    console.log(response.headers.get('post'));
+    console.log(response.headers.get('date'));
+
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.type);
+    console.log(response.url);
+
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+          console.log(response.headers.get('post'))
+        console.log(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+    }
+
+    
     
 
     render() {
         return (
             <div>
-                <h1>Cool post</h1>
-                <p>Hello MlLatestPost! Here is what has hapened in the world of web dev!</p>
+<h1>
+    Title
+    </h1>
+
+    <p>
+Post
+    </p>
+
             </div>
         );
-    }
+      }
+    
 }
