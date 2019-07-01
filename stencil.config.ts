21460dffaf45af903cc0f13583eb49751a759bcc
Bobby Johnson <Bobby@MadnessLabs.net>
2019-07-01 13:57:59 -0500

    Add missing cleave files to assets

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,11 +1,13 @@
-import { Config } from '@stencil/core';
+import { Config } from "@stencil/core";
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
-  outputTargets: [{
-    type: 'www',
-    serviceWorker: null
-  }],
-  globalScript: 'src/global/app.ts',
-  globalStyle: 'src/global/app.css'
+  outputTargets: [
+    {
+      type: "www",
+      serviceWorker: null
+    }
+  ],
+  globalScript: "src/global/app.ts",
+  globalStyle: "src/global/app.css"
 };
