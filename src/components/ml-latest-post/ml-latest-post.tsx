d13e1d8559d005fdc48afb45bf9e464343ac7383
meaustin15 <m.eaustin2015@gmail.com>
2019-07-02 12:09:23 -0500

    latest post update

--- src/components/ml-latest-post/ml-latest-post.tsx
+++ src/components/ml-latest-post/ml-latest-post.tsx
@@ -10,16 +10,42 @@ export class MlLatestPost{
         this.fetchData();
     }
 
-    fetchData = () => {
+    fetchData(){
+
+        // const getPosts = () =>{
+        //     return fetch('https://madnesslabs.net/api/v1/feed')
+        //     .then(res => res.json())
+        //     .then(posts => console.log(posts))
+        // }
+
+
+            const post ={
+                title: 'WordPress Posting for Maximum Reach',
+                body: 'WordPress Posting is Easy if you have the right tools and know-how. Luckily, if you are a customer of Madness Labs&#8217; SAAS you have all of the tools you need to be a pro at SEO with WordPress. In this video I show how to craft a post on your blog, how to optimize your &#8230; <a href=\"https:\/\/blog.madnesslabs.net\/wordpress-posting-for-maximum-reach\/\">Continued<\/a>',
+                userid: 1
+
+            }
+
+        const newPost = posts =>{
+                const options ={
+                    method: 'POST',
+                    body: JSON.stringify(posts),
+                    headers: new Headers({
+                        'content-type' : 'application/json'
+                    })
+                }
+            return fetch('https://madnesslabs.net/api/v1/feed', options)
+            .then(res => res.json())
+            .then(res => console.log(res))
+           
+
+        }
+
+        newPost(post);
+
+    }
    
 
-       let promise =  fetch('https://madnesslabs.net/api/v1/feed', {mode: 'no-cors'});
-        promise.then((res)=>{
-                return res.json();
-        }).then((json)=>{
-            console.log(json)
-        });
-}
     
 
     render() {
